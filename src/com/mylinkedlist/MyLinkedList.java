26e72d373947de9918f0447880d3fa84e8bb9572
nikita <amarnikita09@gmail.com>
2021-12-22 13:39:37 +0530

    refactored MyLinkedList.java to create pop()

--- src/com/mylinkedlist/MyLinkedList.java
+++ src/com/mylinkedlist/MyLinkedList.java
@@ -3,21 +3,36 @@ package com.mylinkedlist;
 import java.util.LinkedList;
 
 public class MyLinkedList {
+	LinkedList<Integer> list = new LinkedList<>();
+	/*
+	 * UC-5:  Ability to remove first element
+	 */
+	public void pop() {
+		list.remove();
+	}
+	
+	public void add(Integer i) {
+		list.add(i);
+	}
+	
+	/*
+	 * UC-4: Ability to insert 30 between 56 and 70
+	 */
+	public void insert(int index, Integer item) {
+		list.add(index, item);
+	}
 	public static void main(String[] args) {
 		
-		/*
-		 * UC-1:created a simple Linked List of 56, 30 and 70
-		 */
-		LinkedList<Integer> list = new LinkedList<>();
-		/*
-		 *Ability to insert 30 between 56 and 70
-		 *Final Sequence: 56->30->70
-		 */
-		list.add(56);
-		list.add(70);
-		System.out.println(list);
-		list.add(1, 30);
-		System.out.println(list);
+		
+		
+		MyLinkedList ls = new MyLinkedList();
+		ls.add(56);
+		ls.add(70);
+		System.out.println(ls.list);
+		ls.insert(1, 30);
+		System.out.println(ls.list);// [56, 30, 70]		
+		ls.pop();
+		System.out.println(ls.list);//[30, 70]
 		
 	}
 }
