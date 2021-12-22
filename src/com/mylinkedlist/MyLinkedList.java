1079569c498253c34a5fca8d7034fc4d1c323741
nikita <amarnikita09@gmail.com>
2021-12-22 12:13:11 +0530

    Refactored as per UC1

--- src/com/mylinkedlist/MyLinkedList.java
+++ src/com/mylinkedlist/MyLinkedList.java
@@ -4,7 +4,13 @@ import java.util.LinkedList;
 
 public class MyLinkedList {
 	public static void main(String[] args) {
+		
+		/*
+		 * UC-1:created a simple Linked List of 56, 30 and 70
+		 */
 		LinkedList<Integer> list = new LinkedList<>();
+		
+		
 		
 	}
 }
