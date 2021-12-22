38c6031b13aa368512b057cbcfb1e1aa99f55294
nikita <amarnikita09@gmail.com>
2021-12-22 12:08:33 +0530

    Added MyLinkedList.java

---
+++ src/com/mylinkedlist/MyLinkedList.java
@@ -0,0 +1,10 @@
+package com.mylinkedlist;
+
+import java.util.LinkedList;
+
+public class MyLinkedList {
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		
+	}
+}
