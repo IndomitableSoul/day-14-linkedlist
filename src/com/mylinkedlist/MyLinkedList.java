61787f4c77d96219b55e2af269ad427490050b48
nikita <amarnikita09@gmail.com>
2021-12-22 12:23:07 +0530

    Refactored MyLinkedList.java as per UC2

--- src/com/mylinkedlist/MyLinkedList.java
+++ src/com/mylinkedlist/MyLinkedList.java
@@ -9,8 +9,14 @@ public class MyLinkedList {
 		 * UC-1:created a simple Linked List of 56, 30 and 70
 		 */
 		LinkedList<Integer> list = new LinkedList<>();
-		
-		
+		/*
+		 * Ability to create Linked List by adding 30 and 56 to 70
+		 * LinkedList Sequence: 56->30->70
+		 */
+		list.add(70);
+		list.addFirst(30);
+		list.addFirst(56);
+		System.out.println(list);
 		
 	}
 }
